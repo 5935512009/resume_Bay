bb3680a30b81b763e44d33a8ee48e4101e530c90
5935512009 <Pasut2541@hotmail.com>
2024-04-30 10:53:18 +0700

    update:{Navbar}

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,12 +14,12 @@ const Navbar = () => {
           <ul className="nav-list">
             <li onClick={()=>{setManu("Home")}} ><Link to="./Home">Home{manu==="Home"?<hr/>:<></>}</Link></li>
             <li onClick={()=>{setManu("About")}}><Link to="./About">About{manu==="About"?<hr/>:<></>}</Link></li>
-            <li onClick={()=>{setManu("Projects")}}><Link to="./Projects">Projects{manu==="Projects"?<hr/>:<></>}</Link></li>
             <li onClick={()=>{setManu("Resume")}}><Link to='./Resume'>Resume{manu==="Resume"?<hr/>:<></>}</Link></li>
+            <li onClick={()=>{setManu("Projects")}}><Link to="./Projects">Projects{manu==="Projects"?<hr/>:<></>}</Link></li>
             <li onClick={()=>{setManu("Blogs")}}><Link to='./Blogs'>Blogs{manu==="Blogs"?<hr/>:<></>}</Link></li>
-            <li onClick={()=>{setManu("Man")}}><Link to='./man'>Man{manu==="Man"?<hr/>:<></>}</Link></li>
+            {/* <li onClick={()=>{setManu("Man")}}><Link to='./man'>Man{manu==="Man"?<hr/>:<></>}</Link></li>
             <li onClick={()=>{setManu("Women")}}><Link to='./women'>Women{manu==="Women"?<hr/>:<></>}</Link></li>
-            <li onClick={()=>{setManu("Child")}}><Link to='./child'>child{manu==="Child"?<hr/>:<></>}</Link></li>
+            <li onClick={()=>{setManu("Child")}}><Link to='./child'>child{manu==="Child"?<hr/>:<></>}</Link></li> */}
             
           </ul>
         </div>
