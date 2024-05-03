23e82caa56e47521ad91097d852347209ad57b9f
5935512009 <Pasut2541@hotmail.com>
2024-05-03 09:39:39 +0700

    update:{front-end}

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -1,30 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import './blog.css';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import "./blog.css";
+import axios from "axios";
 
 export default function Blogs() {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
-    axios.get('https://your-app-name.vercel.app/todos') // ส่งคำขอ HTTP ไปยัง API ของ back-end ที่ endpoint '/api/todos'
-      .then(res => {
+    axios
+      .get("http://localhost:5000/todos") // ส่งคำขอ HTTP ไปยัง API ของ back-end ที่ endpoint '/api/todos'
+      .then((res) => {
         setItems(res.data);
         console.log(res.data);
       })
-      .catch(error => {
-        console.log('hello this error is .....',error);
+      .catch((error) => {
+        console.log("hello this error is .....", error);
       });
-      
   }, []);
 
   return (
-    <div className='blogs'>
+    <div className="blogs">
       <h3>Items</h3>
-      {items.map((item, index) => (
-        <div key={index}>
-          <p>{item.description}</p>
-        </div>
-      ))}
+      <div className="blogs-items">
+        {items.map((item, index) => (
+          <div className="blogs-items-lists" key={index}>
+            <p className="name">NAME: {item.name}</p>
+            <p>DESCRIPTION : {item.description}</p>
+            <p>TOOLS : {item.tools}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
