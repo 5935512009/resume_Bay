bf783034a6782028776e46ad9538dd8ccdcfe3ad
5935512009 <Pasut2541@hotmail.com>
2024-05-15 11:57:57 +0700

    update:{allow method delete}

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 // Middleware to handle CORS
 app.use(cors({
   origin: ['https://resume-bay-full-stack.vercel.app','https://resume-bay-full-stack-5935512009s-projects.vercel.app','https://resume-bay-seven.vercel.app','http://localhost:3000'], // Allow requests from specific origin
-  methods: ['GET','POST','PUT'], // Allow only GET requests
+  methods: ['GET','POST','PUT','DELETE'], // Allow only GET requests
   credentials: true // Allow credentials such as cookies to be sent along with requests
 }));
 
